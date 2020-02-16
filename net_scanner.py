ffb0a70a757b966832f2a07efa5cfb41f9387961
Artem Semenchenko <jindnbua@gmail.com>
2020-02-16 17:13:01 -0500

    network scanner

--- net_scanner.py
+++ net_scanner.py
@@ -2,9 +2,30 @@
 
 import scapy.all as scapy
 
-# Use ARP protocol 
-def scan(ip):
-  scapy.arping(ip)
 
-scapy("192.168.0.1/24")
- 
+# def scan(ip):
+#     scapy.arping(ip)
+
+
+# Use ARP protocol
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    brod_cast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_brodcast_request = brod_cast / arp_request
+    # answered this is who use this network
+    answered = scapy.srp(arp_brodcast_request, timeout=1, verbose=False)[0]
+    return answered
+
+
+def output_scanned_network(answered_list):
+    print("IP\t\t\t\tMAC\n__________________________________________________")
+    for item in answered_list:
+        # print(item[1].show())
+        answered_ip_address = item[1].psrc
+        answered_mac_address = item[1].hwsrc
+        print(answered_ip_address + "\t\t\t" + answered_mac_address)
+
+
+list = scan("10.0.2.1/24")
+
+output_scanned_network(list)
