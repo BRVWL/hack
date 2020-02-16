caf82126be6b8699430a43a4de6e048795ab8015
Artem Semenchenko <jindnbua@gmail.com>
2020-02-16 17:30:50 -0500

    refactoring

--- net_scanner.py
+++ net_scanner.py
@@ -14,16 +14,23 @@ def scan(ip):
     arp_brodcast_request = brod_cast / arp_request
     # answered this is who use this network
     answered = scapy.srp(arp_brodcast_request, timeout=1, verbose=False)[0]
-    return answered
+    clients_list = []
+    for item in answered:
+        # print(item[1].show())
+        answered_ip_address = item[1].psrc
+        answered_mac_address = item[1].hwsrc
+        current_client = {
+          "ip": answered_ip_address,
+          "mac": answered_mac_address
+        }
+        clients_list.append(current_client)
+    return clients_list
 
 
 def output_scanned_network(answered_list):
     print("IP\t\t\t\tMAC\n__________________________________________________")
-    for item in answered_list:
-        # print(item[1].show())
-        answered_ip_address = item[1].psrc
-        answered_mac_address = item[1].hwsrc
-        print(answered_ip_address + "\t\t\t" + answered_mac_address)
+    for client in answered_list:
+        print(client["ip"] + "\t\t\t" + client["mac"])
 
 
 list = scan("10.0.2.1/24")
